19ed77af685e4dc3866361c8a96d4ba4590717d7
nsym7372 <nsym7372@outlook.com>
2020-09-20 12:26:28 +0900

    画面遷移できた

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def upload(request):
+    if request.method == "POST":
+        # form = BookForm(request.POST)
+        # if form.is_valid():
+        #     book = Book()
+        #     print(request)
+        #     book.title = request.POST['title']
+        #     book.link = request.POST['link']
+        #     book.image = request.FILES['image']
+        #     book.author = request.user
+        #     book.published_date = timezone.now()
+        #     book.save()
+        return redirect('result')
+    else:
+        # form = BookForm()
+    # return render(request, 'blog/new.html', {'form': form})
+        return redirect('index')
+
+def result(request):
+    return render(request, 'result.html')
