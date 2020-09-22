7e8cb90ad1b379217a38b4ec1d2b57b5ad86a3bc
nsym7372 <nsym7372@outlook.com>
2020-09-22 23:52:36 +0900

    モデル読み込みを実装（まだ動作しない）

---
+++ firstapp/cnn_model.py
@@ -0,0 +1,76 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+import pytorch_lightning as pl
+from pytorch_lightning import Trainer
+
+class TrainNet(pl.LightningModule):
+  
+  # def train_dataloader(self):
+  #   return torch.utils.data.DataLoader(train, self.batch_size, shuffle=True, num_workers=self.num_workers)
+
+  def training_step(self, batch, batch_idx):
+    x, t = batch
+    y = self.forward(x)
+    loss = self.lossfun(y, t)
+    results = {'loss': loss}
+    return results
+
+class ValidationNet(pl.LightningModule):
+
+#   @pl.data_loader
+#   def val_dataloader(self):
+#     return torch.utils.data.DataLoader(val, self.batch_size, num_workers=self.num_workers)
+
+  def validation_step(self, batch, batch_idx):
+    x, t = batch
+    y = self.forward(x)
+    loss = self.lossfun(y, t)
+    y_label = torch.argmax(y, dim=1)
+    acc = torch.sum(t == y_label) * 1.0 / len(t)
+    results = {'val_loss': loss, 'val_acc': acc}
+    return results
+
+  def validation_end(self, outputs):
+    avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+    avg_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
+    results = {'val_loss': avg_loss, 'val_acc': avg_acc}
+    return results
+
+class Net(TrainNet, ValidationNet):
+
+  def __init__(self):
+    super().__init__()
+
+    # # バッチサイズ
+    # self.batch_size = batch_size
+
+    # 画像解像度　torch.Size([3, 128, 128])
+    self.conv = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1) # (128 + 2 - 3 / 1) + 1 = 128
+    self.pool = nn.MaxPool2d(2, 2) # (128 - 2 / 2) + 1 = 64
+    self.bn = nn.BatchNorm1d(3 * 64 * 64)  # 64 * 64 ピクセルの画像、特徴マップ3枚
+    self.fc1 = nn.Linear(3 * 64 * 64, 100)
+    self.fc2 = nn.Linear(100, 4)
+
+
+  def lossfun(self, y, t):
+    return F.cross_entropy(y, t)
+
+  def configure_optimizers(self):
+    return torch.optim.Adam(self.parameters(), lr=0.01,  weight_decay=0.001)
+
+  def forward(self, x):
+    x = self.conv(x)
+    x = F.relu(x)
+    x = self.pool(x)
+
+    x = x.view(x.size(0), -1)
+    x = self.bn(x)
+    x = self.fc1(x)
+    x = F.relu(x)
+    x = self.fc2(x)
+    return x
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,16 @@
+from numpy.core.fromnumeric import argmax
 from firstapp.models import Animal
 from django.shortcuts import render, redirect
 from .forms import AnimalForm
+#from sklearn.externals import joblib
+import joblib
+from torchvision import transforms
+from scipy.special import softmax
+from .cnn_model import Net
+import torch
 # Create your views here.
+
+# loaded_model = joblib.load('firstapp/trained_model.pkl')
 
 def index(request):
     return render(request, 'index.html')
@@ -19,4 +28,41 @@ def upload(request):
 
 def result(request):
     data = Animal.objects.order_by('id').reverse()
+
+# path = 'index.jpg'
+# img2 = Image.open(path)
+# img2
+
+# mt = transforms.Compose([
+#         transforms.Resize((128, 128)),
+#         transforms.ToTensor(),
+#     ])
+
+#     img3 = mt(img2)
+
+#     ret = F.softmax(model(img3.unsqueeze(0)))
+# print(ret)
+# ret.argmax()
+    model = Net()
+    model.load_state_dict(torch.load('firstapp/trained.pth', map_location=torch.device('cpu')))
+
+    mt = transforms.Compose([
+            transforms.Resize((128, 128)),
+            transforms.ToTensor(),
+        ])
+    
+    transformed = mt(data[0].photo)
+    ret = softmax(model(transformed.unsqueeze(0)))
+    
+
+
+
+    # x = np.array([data[0]])
+    # y = loaded_model.predict(x)
+    # y_proba = loaded_model.predict_proba(x) * 100 # 100分率
+    # y, y_proba = y[0], y_proba[0] # 配列からスカラーに変更
+
+    # # 結果に応じた方のみ確率表示
+    # proba = round(y_proba[y], 2)
+
     return render(request, 'result.html', {'photo':  data[0].photo})
